17f7e875709cb264445fb1f4682ecae78bc22c74
OscarM03 <oscarmutwiri03@gmail.com>
2023-10-17 08:32:42 +0300

    fixed errors

--- main.c
+++ main.c
@@ -1,22 +1,25 @@
 #include "shell.h"
 /**
- * main - Entry point function that runs the main program.
- *
- * Return: Always 0.
+ * main_loop - Main loop of the shell program.
+ * @context: Pointer to a Shell context structure.
+ * This function continuously prompts for user input,
+ * processes commands,
+ * and handles special commands like status and PID.
+ * It uses the provided
+ * Shell context to keep track of the shell's state.
  */
-
-int main(void)
+void main_loop(Shell *context)
 {
 	char *command;
-	Shell context = {0};
 
 	while (1)
 	{
 		shell_prompt();
 		command = user_entries();
+
 		if (my_strstr(command, "$?"))
 		{
-			handle_status(&context);
+			handle_status(context);
 		}
 		else if (my_strstr(command, "$$"))
 		{
@@ -24,11 +27,34 @@ int main(void)
 		}
 		else
 		{
-			process_entries(command, &context);
+			process_entries(command, context);
 		}
+
 		free(command);
 	}
-
-	return (0);
 }
 
+/**
+ * main - Entry point of the shell program.
+ * This function initializes the Shell context and
+ * enters the main loop.
+ * It checks if input is coming from a terminal,
+ * and if so, starts the
+ * interactive shell by calling main_loop.
+ *
+ * Return: Always returns 0.
+ */
+int main(void)
+{
+	Shell context = {0};
+
+	if (isatty(STDIN_FILENO))
+	{
+		main_loop(&context);
+	}
+	else
+	{
+		main_loop(&context);
+	}
+	return (0);
+}
