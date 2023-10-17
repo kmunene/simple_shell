e4afa80839ab12771b8b9c11e130878ee72ef9d9
OscarM03 <oscarmutwiri03@gmail.com>
2023-10-17 09:17:22 +0300

    fixed errors

--- main.c
+++ main.c
@@ -1,60 +1,47 @@
 #include "shell.h"
 /**
- * main_loop - Main loop of the shell program.
- * @context: Pointer to a Shell context structure.
- * This function continuously prompts for user input,
- * processes commands,
- * and handles special commands like status and PID.
- * It uses the provided
- * Shell context to keep track of the shell's state.
- */
-void main_loop(Shell *context)
-{
-	char *command;
-
-	while (1)
-	{
-		shell_prompt();
-		command = user_entries();
-
-		if (my_strstr(command, "$?"))
-		{
-			handle_status(context);
-		}
-		else if (my_strstr(command, "$$"))
-		{
-			handle_pid();
-		}
-		else
-		{
-			process_entries(command, context);
-		}
-
-		free(command);
-	}
-}
-
-/**
- * main - Entry point of the shell program.
- * This function initializes the Shell context and
- * enters the main loop.
- * It checks if input is coming from a terminal,
- * and if so, starts the
- * interactive shell by calling main_loop.
+ * main - Entry point of the shell program
  *
- * Return: Always returns 0.
+ * Return: Always returns 0 to indicate successful execution
  */
 int main(void)
 {
+	char *command;
 	Shell context = {0};
 
 	if (isatty(STDIN_FILENO))
 	{
-		main_loop(&context);
+		while (1)
+		{
+			shell_prompt();
+			command = user_entries();
+			if (my_strstr(command, "$?"))
+			{
+				handle_status(&context);
+			} else if (my_strstr(command, "$$"))
+			{
+				handle_pid();
+			} else
+			{
+				process_entries(command, &context);
+			}
+			free(command);
+		}
 	}
 	else
 	{
-		main_loop(&context);
+		command = user_entries();
+		if (my_strstr(command, "$?"))
+		{
+			handle_status(&context);
+		} else if (my_strstr(command, "$$"))
+		{
+			handle_pid();
+		} else
+		{
+			process_entries(command, &context);
+		}
+		free(command);
 	}
 	return (0);
 }
--- shell_prompt.c
+++ shell_prompt.c
@@ -4,16 +4,11 @@
  */
 void shell_prompt(void)
 {
-	if (isatty(STDIN_FILENO))
-	{
-		int i;
-		char prompt[] = "odec$ ";
+	int i;
+	char prompt[] = "odec$ ";
 
-		for (i = 0; prompt[i] != '\0'; i++)
-		{
-			my_print(prompt[i]);
-		}
-		fflush(stdout);
+	for (i = 0; prompt[i] != '\0'; i++)
+	{
+		my_print(prompt[i]);
 	}
 }
-
